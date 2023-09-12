7d95e8ef0f949456d3dbb05cbac692b38a8ffa02
JoshuaNegedu <JoshuaNegedu>
2023-09-12 05:18:53 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main();{
+	char text[] = "_putchar"
+		int i
+		for (i=0; text[i] != '\0'; i++) {
+			putchar(text[i]);
+		}
+	    putchar('\n'); // Print a new line character
+	    return (0);
+}
